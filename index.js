b15a927a8e6fc7d0dfb2880317219cd9ebe26765
Idhanurasanti <151720371+Idhanurasanti@users.noreply.github.com>
2023-12-13 14:51:43 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,94 +1,16 @@
-//variable
+const express = require("express");
+const app = express();
+const port = 8000;
+const apiRouter = require("./api-routes")
+
+app.get("/", (req, res) => {
+  res.send("hello , Selamat anda telah berhasil membuat server dengan express");
+});
+
+app.use ("/api" , apiRouter);
 
 
-// // var
-// var greeter = "hey hay";
-// var greeter = "hey helo";
-// console.log(greeter);
-// //let
 
-// let greeter2 = "hey hay";
-// let status = true;
-// if (status = true) {
-//      let greeter2 = "hey helo" ;
-// }
-// console.log(greeter2);
-
-//const
-
-// const greeter3 = "hey hay";
-// greeter3 = "hey helo";
-
-// console.log(greeter3);
-
-//flow for
-let angka = "";
-// for (let i = 0; i <= 9; i++) {
-//     angka = angka + i ;
-// }
-
-// console.log(angka);
-
-//while
-// while (angka < 9)
-// {angka++
-//     console log(angka);
-// }
-
-
-//do while
-// let i=0 ;
-
-// do {
-//     i = i + i;
-//     angka = angka + i
-// }while (i < 9)
-// console.log(angka);
-
-// function testNum(){
-//     let angka ;
-//     if (a> 0)
-//     { angka  = "positive"}
-//     else
-//     { angka  = "Negative"}
-//     return angka
-
-
-// }
-console.log(testNum(-5))
-
-const fruit = "papayas"
-
-// switch(fruit)
-//         { 
-//             case "oranges" :
-//             {console.log(" 1kg oranges are 15.000 IDR ")
-//             break;
-//         }
-//         case "mangoes" :
-//             {console.log(" 1kg mangoes are 20.000 IDR ")
-//             break;
-
-//         }
-//         case "papayas" :
-//             {console.log(" 1kg papayas are 9.000 IDR ")
-//             break;
-
-//         }
-
-//         default :
-//         console.log("sorry , out of order")
-//     }
-
-let fruits = ["banana" , "mango" , "papaya"]
-
-// if (fruits.length > 0)
-// {console.log(fruits)}
-// else{console.log("data kosong")}
-
-// fruits.forEach( function (item , index ,array){
-//     console.log(item,index)
-// })
-
-let addfruit = fruits.shift()
-console.log(fruits)
+app.listen(port, () => {
+   console.log|(`server berjalan di port ${port}`);
+});
